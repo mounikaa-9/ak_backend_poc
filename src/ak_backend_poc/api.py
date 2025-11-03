e829095d83b23321c65de0e47646f153a216db97
dt-tanmay <tanmay@dataterminal.co>
2025-11-03 16:00:52 +0530

    Adding guard function for blocking API re-registry

--- src/ak_backend_poc/api.py
+++ src/ak_backend_poc/api.py
@@ -4,10 +4,23 @@ from ninja_jwt.controller import NinjaJWTDefaultController
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
 
-# Register all routers here
-api.add_router("/users", "users.api.users_router")
-api.add_router("/heatmaps", "heatmaps.api.heatmaps_router")
-api.add_router("/ai_advisory", "ai_advisory.api.ai_advisory_router")
-api.add_router("/weather", "weather.api.weather_router")
-api.add_router("/pipelines", "pipelines.new_profile_script.creation_router")
-api.add_router("/crop_loss_analytics", "crop_loss_analytics.api.crop_loss_analytics_router")
+# Guard to prevent duplicate registration
+_routers_registered = False
+
+def register_routers():
+    global _routers_registered
+    if _routers_registered:
+        return
+    
+    # Register all routers here
+    api.add_router("/users", "users.api.users_router")
+    api.add_router("/heatmaps", "heatmaps.api.heatmaps_router")
+    api.add_router("/ai_advisory", "ai_advisory.api.ai_advisory_router")
+    api.add_router("/weather", "weather.api.weather_router")
+    api.add_router("/pipelines", "pipelines.new_profile_script.creation_router")
+    api.add_router("/crop_loss_analytics", "crop_loss_analytics.api.crop_loss_analytics_router")
+    
+    _routers_registered = True
+
+# Call the function
+register_routers()
